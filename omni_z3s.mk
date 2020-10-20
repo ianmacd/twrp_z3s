6301ac2592725cc0b6522f72eaec6d87cf89583a
Ian Macdonald <ian@caliban.org>
2020-10-20 10:49:30 +0200

    Use PRODUCT_USE_DYNAMIC_PARTITIONS to set ro.boot.dynamic_partitions.

--- omni_z3s.mk
+++ omni_z3s.mk
@@ -1,8 +1,8 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Include any options that can't be included in BoardConfig.mk
+$(call inherit-product, device/samsung/z3s/device.mk)
 
 # Inherit from those products. Most specific first.
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)  # If you are building for a tablet
@@ -10,5 +10,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := omni_z3s
 PRODUCT_DEVICE := z3s
+PRODUCT_MODEL := SM-F988B
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
